560bbc74bb45315075cff00e66f9657cd4defc43
Otavio Senne <otavio.senne@gmail.com>
2024-11-15 11:06:51 -0300

    Terminando videos Maquina

--- scripts/maquina.js
+++ scripts/maquina.js
@@ -2,10 +2,12 @@ function mostrarIframe(id) {
     document.getElementById(id).classList.remove('invisible');
     document.getElementById(id).classList.remove('hidden');
     document.getElementById(id).classList.add('visible');
-    document.getElementById(id).classList.add('block');
+    document.getElementById(id).classList.add('flex');
   }
 
   function esconderIframe(id) {
-    document.getElementById(id).classList.remove('block');
+    document.getElementById(id).classList.remove('visible');
+    document.getElementById(id).classList.remove('flex');
+    document.getElementById(id).classList.add('invisible');
     document.getElementById(id).classList.add('hidden');
   }
